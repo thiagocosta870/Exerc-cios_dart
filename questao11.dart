4d211e37494f3695283ab4b6e2adc7da463c34d3
thiagocosta870 <titocosta416@gmail.com>
2026-06-19 13:14:44 -0300

    atualizacoes

--- questao11.dart
+++ questao11.dart
@@ -7,5 +7,22 @@ import 'dart:math';
 
 void main() {
 
-  int numero = Random.nextInt(101);
+  int numero = Random().nextInt(101);
+
+  int tentativasUsuario = 0;
+  
+  for (int chute = 1; chute <= 100; chute ++){
+    tentativasUsuario++;
+    print('tentativa $tentativasUsuario, chutei $chute ');
+
+    if(chute == numero) {
+      print('Voce acertou, o numero que eu estou pensando é $numero');
+      break;
+    }else if(chute < numero) {
+      print('Voce chutou um numero muito baixo, tente novamente');
+    }else{
+      print('Voce chutou um numero muito alto, tente novamente');
+    }
+  }
+  print('Voce realizou $tentativasUsuario tentativas');
 }
