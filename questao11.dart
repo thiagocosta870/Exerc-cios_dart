29f04b8508d5c3e434b77b8045747a6d5d506d84
thiagocosta870 <titocosta416@gmail.com>
2026-06-19 11:33:45 -0300

    Atualizacoes

---
+++ questao11.dart
@@ -0,0 +1,11 @@
+// 11 - Gere um número aleatório entre 1 e 100. Peça para o usuário adivinhar o número e,
+// em seguida, diga se ele chutou muito baixo, muito alto ou se acertou completamente.
+// Registre quantas tentativas o usuário fez e, quando o jogo terminar, imprima isso.
+
+import 'dart:math';
+
+
+void main() {
+
+  int numero = Random.nextInt(101);
+}
